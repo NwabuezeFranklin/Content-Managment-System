f8fdbfa8200a23ac465678debb13f6a23d98735d
NwabuezeFranklin <nwafrank2017@gmail.com>
2023-04-10 17:58:17 +0100

    Error testing

--- Base/settings.py
+++ Base/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%wnrzh)ik)(q%cv_8l2=*-1xy1j_njh4hjvb+wp24mlvznp+jk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
