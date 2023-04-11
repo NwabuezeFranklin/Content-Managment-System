f35747a40a6707295f42a55491557665f33a4001
NwabuezeFranklin <nwafrank2017@gmail.com>
2023-04-11 13:17:33 +0100

    Data compressing

--- Base/urls.py
+++ Base/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from App import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
