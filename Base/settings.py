6fa5f07034d70e7b9851d451147e57b7997edcbb
NwabuezeFranklin <nwafrank2017@gmail.com>
2023-04-14 04:30:40 +0100

    media

--- Base/settings.py
+++ Base/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 
@@ -164,12 +164,12 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
-CKEDITOR_UPLOAD_PATH = 'static/'
+CKEDITOR_UPLOAD_PATH = 'media/'
 
 
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
