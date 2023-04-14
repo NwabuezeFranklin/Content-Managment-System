9e9a8bfa888e0fe2d5b715424de2480ab848caf2
NwabuezeFranklin <nwafrank2017@gmail.com>
2023-04-14 17:06:50 +0100

    Media_URL

--- Base/settings.py
+++ Base/settings.py
@@ -172,6 +172,8 @@ MEDIA_URL = '/media/'
 #MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_ROOT = BASE_DIR / 'static/images' 
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
