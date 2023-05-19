ff055920fe42a7fd085c79936c34a9a137dc8291
Nwabueze Franklin <nwafrank2017@gmail.com>
2023-05-19 21:45:39 +0100

    Update settings.py

--- Base/settings.py
+++ Base/settings.py
@@ -21,6 +21,7 @@ import cloudinary_storage
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
