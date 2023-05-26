9388055f3ffbd430553347065cab0234c2d61907
Nwabueze Franklin <nwafrank2017@gmail.com>
2023-05-26 21:39:11 +0100

    Update settings.py

--- Base/settings.py
+++ Base/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+
 import os
 
 from pathlib import Path
