55fbb00282cc8f59841e22102eb3115faaef2334
Nwabueze Franklin <nwafrank2017@gmail.com>
2023-06-08 07:02:18 +0100

    Update urls.py

--- Base/urls.py
+++ Base/urls.py
@@ -1,6 +1,7 @@
 """Base URL Configuration
 
 
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
