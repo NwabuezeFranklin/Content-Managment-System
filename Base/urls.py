44e9856a8e4138b20641172bdd78b5d823832eb8
Nwabueze Franklin <nwafrank2017@gmail.com>
2023-06-09 20:47:34 +0100

    Update urls.py

--- Base/urls.py
+++ Base/urls.py
@@ -15,6 +15,7 @@ Class-based views
 Including another URLconf
 
     1. Import the include() function: from django.urls import include, path
+    
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
     
 """
